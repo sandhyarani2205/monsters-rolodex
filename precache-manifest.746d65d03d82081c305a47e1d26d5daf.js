820e549a5c09e1b72f73130196a53d9af6b384fc
sandhyarani <sandhyaranihegde2205@gmail.com>
2020-06-21 21:28:15 +0200

    Updates

---
+++ precache-manifest.746d65d03d82081c305a47e1d26d5daf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b86038406ed8aef623857af78d6756d7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1576081bf1a18266c2c3",
+    "url": "/monsters-rolodex/static/css/main.74ec7c24.chunk.css"
+  },
+  {
+    "revision": "0105f5fa7a131123a102",
+    "url": "/monsters-rolodex/static/js/2.8a77c072.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.8a77c072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1576081bf1a18266c2c3",
+    "url": "/monsters-rolodex/static/js/main.ab13b913.chunk.js"
+  },
+  {
+    "revision": "bc9b3e30e5d8354d0e21",
+    "url": "/monsters-rolodex/static/js/runtime-main.bdaefb98.js"
+  }
+]);
